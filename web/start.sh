baa99d15613c7728f4f7be474a28ae682f8d703f
strugglingyouth <630441839@qq.com>
2016-08-02 16:50:57 +0800

    no css

--- web/start.sh
+++ web/start.sh
@@ -4,7 +4,7 @@ docker exec -d mysql mysql -uroot -p123456  blog < blog.sql
 docker build -t feiyu/django-app .
 docker run --name django \
 -v /usr/src/myblog \
--v /usr/src/myblog/blog/static \
+-v /usr/src/myblog/static \
 --link mysql:mysql \
 --link redis:redis \
 -p 12000:8000 \
