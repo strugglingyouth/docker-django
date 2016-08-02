7dd2c22e606796a99f78e09e76e17afb6762633d
strugglingyouth <630441839@qq.com>
2016-08-02 10:55:19 +0800

    add blog file

--- web/init_django.sh
+++ web/init_django.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo '---------------migrate database main------------------------'
-docker exec -it django python manage.py migrate
-echo '---------------create superuser admin-----------------------'
-docker exec -it django python manage.py createsuperuser --username admin --email admin@admin.com --database default
+#docker exec -it django python manage.py migrate
+
+#echo '---------------create superuser admin-----------------------'
+#docker exec -it django python manage.py createsuperuser --username admin --email admin@admin.com --database default
